667da69c5359f76659e0cc4b3eaa7327d1c848db
aillieo <aillieo@163.com>
2020-10-23 20:14:59 +0800

    add profiler

---
+++ Assets/EasyObjectPool/Editor/PoolProfilerWindow.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace AillieoUtils
+{
+    public class PoolProfilerWindow : EditorWindow
+    {
+
+        [MenuItem("AillieoUtils/EasyObjectPool/Profiler")]
+        public static void Open()
+        {
+            GetWindow<PoolProfilerWindow>("EasyObjectPool Profiler");
+        }
+
+        private readonly List<PoolProfiler.ProfilerInfo> records = new List<PoolProfiler.ProfilerInfo>();
+
+
+        private GUILayoutOption[] widthControl_150;
+        private GUILayoutOption[] widthControl_100;
+
+        private void OnEnable()
+        {
+            widthControl_150 = new GUILayoutOption[] { GUILayout.ExpandWidth(false), GUILayout.Width(150) };
+            widthControl_100 = new GUILayoutOption[] { GUILayout.ExpandWidth(false), GUILayout.Width(100) };
+        }
+
+        private void OnGUI()
+        {
+            PoolProfiler.RetrieveRecords(records);
+
+            if(records.Count == 0)
+            {
+                GUILayout.Label("No active pool found.");
+                return;
+            }
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Name", widthControl_150);
+            GUILayout.Label("Type", widthControl_150);
+            GUILayout.Label($"Create", widthControl_100);
+            GUILayout.Label($"Get", widthControl_100);
+            GUILayout.Label($"Recycle", widthControl_100);
+            GUILayout.Label($"Destroy", widthControl_100);
+            GUILayout.EndHorizontal();
+
+            foreach(var i in records)
+            {
+                GUILayout.BeginHorizontal("box");
+                GUILayout.Label(i.name, widthControl_150);
+                GUILayout.Label(i.type, widthControl_150);
+                GUILayout.Label($"{i.timesCreate}", widthControl_100);
+                GUILayout.Label($"{i.timesGet}", widthControl_100);
+                GUILayout.Label($"{i.timesRecycle}", widthControl_100);
+                GUILayout.Label($"{i.timesDestroy}", widthControl_100);
+                GUILayout.EndHorizontal();
+            }
+        }
+
+
+    }
+
+}
---
+++ Assets/EasyObjectPool/Scripts/PoolProfiler.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AillieoUtils
+{
+    public static class PoolProfiler
+    {
+        public enum PoolAction
+        {
+            Create,
+            Get,
+            Recycle,
+            Destroy,
+        }
+
+        public class ProfilerInfo
+        {
+            internal ProfilerInfo(string name, string type)
+            {
+                this.name = name;
+                this.type = type;
+            }
+
+            public string name { get; private set; }
+            public string type { get; private set; }
+            public int timesCreate { get; internal set; }
+            public int timesGet { get; internal set; }
+            public int timesRecycle { get; internal set; }
+            public int timesDestroy { get; internal set; }
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void Report<T>(Pool<T> pool, PoolAction action)
+        {
+            ProfilerInfo info;
+            if (!records.TryGetValue(pool, out info))
+            {
+                info = new ProfilerInfo(pool.ToString(), typeof(T).Name);
+                records.Add(pool, info);
+            }
+            switch(action)
+            {
+                case PoolAction.Create:
+                    info.timesCreate++;
+                    break;
+                case PoolAction.Get:
+                    info.timesGet++;
+                    break;
+                case PoolAction.Recycle:
+                    info.timesRecycle++;
+                    break;
+                case PoolAction.Destroy:
+                    info.timesDestroy++;
+                    break;
+            }
+        }
+
+        private static readonly Dictionary<object, ProfilerInfo> records = new Dictionary<object, ProfilerInfo>();
+
+        public static void RetrieveRecords(List<ProfilerInfo> infoList)
+        {
+            infoList.Clear();
+            foreach(var pair in records)
+            {
+                infoList.Add(pair.Value);
+            }
+        }
+    }
+
+}
